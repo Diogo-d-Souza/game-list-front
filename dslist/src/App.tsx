2b74f0b3675b11aa30d47d752861fa14231d2f3d
Diogo de Souza <di_smgomes@hotmail.com>
2023-05-13 23:25:28 -0300

    project created

---
+++ dslist/src/App.tsx
@@ -0,0 +1,16 @@
+import './App.css'
+import Lista from './components/lista'
+
+function App() {
+
+  return (
+    <>
+      <div className='container'>
+        <h1>Lista de jogos</h1>
+      </div>
+      <Lista/>
+    </>
+  )
+}
+
+export default App
---
+++ dslist/src/components/lista.tsx
@@ -0,0 +1,11 @@
+import './list.css'
+
+export default function Lista(){
+    return(
+        <div className="cor">
+            <h1>
+                Juju é cornos
+            </h1>
+        </div>
+    )
+}
